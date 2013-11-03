e6c72c35f69de1daa4f861f5359f9993614e12a6
danielguerber <guerbi1990@gmx.ch>
2013-11-03 12:25:26 +0100

    Finished Implementation

--- HashLab/src/algorithm/HashAlgorithm.java
+++ HashLab/src/algorithm/HashAlgorithm.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import org.bouncycastle.crypto.engines.DESEngine;
@@ -8,31 +9,46 @@ import org.bouncycastle.crypto.params.DESParameters;
 
 
 public class HashAlgorithm {
+	private final static byte[] IV = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
+	
+	private static byte[] addPaddingAndLength(byte[] message) {
+		int paddingLength = 8 - (message.length % 8);
+		
+		byte[] newMessage = new byte[message.length + paddingLength + 8];
+		
+		System.arraycopy(message, 0, newMessage, 0, message.length);
+		
+		if (message.length % 8 > 0) {
+			newMessage[message.length] = -128;
+		}
+		
+		System.arraycopy(ByteBuffer.allocate(8).putLong(message.length).array(), 0, newMessage, newMessage.length - 8, 8);
+	
+		return newMessage;
+	}
+	
 	public static byte[] calculateHash(byte[] message) {
 		DESEngine engine = new DESEngine();
-		byte[] g = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
 		
-		for (int i = 0; i < message.length; i+=8) {
-			byte[] m;
+		byte[] newMessage = addPaddingAndLength(message);
+		
+		byte[] prevHash = IV;
+		
+		for (int i = 0; i < newMessage.length; i+=8) {
+			byte[] m = Arrays.copyOfRange(newMessage, i, i+8);
 			
-			if (i+8 < message.length) {
-				m = Arrays.copyOfRange(message, i, i+8);
-			} else {
-				m = new byte[64];
-				System.arraycopy(message, i, m, 0, message.length - i);
-				m[message.length - i] = 8;
-			}
-			engine.init(true, new DESParameters(g));
+			engine.init(true, new DESParameters(prevHash));
 			byte[] tmp = new byte[8];
 			engine.processBlock(m,0,tmp,0);
-			for (int j = 0; j < g.length; j++) {
-				g[j] = (byte)(g[j] ^ tmp[j]);
+			
+			for (int j = 0; j < prevHash.length; j++) {
+				prevHash[j] = (byte)(prevHash[j] ^ tmp[j]);
 			}
 		}
 		
 		byte[] result = new byte[4];
 		for (int i = 0; i < result.length; i++) {
-			result[i] = (byte)(g[i] ^ g[7-i]);
+			result[i] = (byte)(prevHash[i] ^ prevHash[7-i]);
 		}
 		return result;
 	}
--- HashLab/src/generator/Generator.java
+++ HashLab/src/generator/Generator.java
@@ -7,15 +7,113 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-
-import org.bouncycastle.util.Arrays;
+import java.util.Map.Entry;
 
 import algorithm.HashAlgorithm;
 
-public class Generator {
-	public static String[][] readReplacements() throws IOException {
+public final class Generator {
+	
+	private final int maxCollisions;
+	private final String fileOriginal;
+	private final String templateOriginal;
+	private final String templateCopy;
+	private final String[][] replacements;
+	private final boolean changeOriginal;
+	
+	public Generator(int maxCollisions, boolean changeOriginal) throws IOException {
+		this.maxCollisions = maxCollisions;
+		this.changeOriginal = changeOriginal;
+		
+		this.fileOriginal = readFile("OriginalFile.txt");
+		this.templateOriginal = readFile("PlaceholderOriginal.txt");
+		this.templateCopy = readFile("PlaceholderCopy.txt");
+		
+		replacements = readReplacements();
+		
+		searchCollisions();
+	}
+	
+	private void searchCollisions() throws FileNotFoundException {
+		HashMap<Integer, String> originalHashes = new HashMap<Integer, String>();
+		HashMap<Integer, String> copyHashes = new HashMap<Integer, String>();
+		
+		int foundCollisions = 0;
+		HashSet<Integer> collidedHashes = new HashSet<Integer>();
+		
+		ByteBuffer buffer = ByteBuffer.wrap(HashAlgorithm.calculateHash(fileOriginal.getBytes()));
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        int origHash = buffer.getInt();
+		
+		originalHashes.put(origHash, fileOriginal);
+		
+		while (foundCollisions < maxCollisions) {
+			for (int i = 0; i < 2048; i++) {
+				String randomCopy = String.format(templateCopy, getRandomReplacement());
+				buffer = ByteBuffer.wrap(HashAlgorithm.calculateHash(randomCopy.getBytes()));
+		        buffer.order(ByteOrder.LITTLE_ENDIAN);
+		        int copyHash = buffer.getInt(); 
+
+		        copyHashes.put(copyHash, randomCopy);
+		        
+		        if (changeOriginal)
+		        {
+		        	String randomOriginal = String.format(templateOriginal, getRandomReplacement());
+					buffer = ByteBuffer.wrap(HashAlgorithm.calculateHash(randomOriginal.getBytes()));
+			        buffer.order(ByteOrder.LITTLE_ENDIAN);
+			        origHash = buffer.getInt(); 
+
+			        originalHashes.put(origHash, randomOriginal);
+		        }
+			}
+			
+			for (Entry<Integer,String> hashEntry : originalHashes.entrySet()) {
+				if (!collidedHashes.contains(hashEntry.getKey()) && copyHashes.containsKey(hashEntry.getKey())) {
+					foundCollisions++;
+					collidedHashes.add(hashEntry.getKey());
+					PrintWriter out = new PrintWriter("original" + foundCollisions + ".txt");
+					out.print(hashEntry.getValue());
+					out.close();
+					
+					out = new PrintWriter("copy" + foundCollisions + ".txt");
+					out.print(copyHashes.get(hashEntry.getKey()));
+					out.close();
+				}
+			}	
+			
+			System.out.println("Calculating...");
+			System.out.println("Number of Collisions: "  + foundCollisions);
+			System.out.println("Number of original Hashes: " + originalHashes.size());
+			System.out.println("Number of copy Hashes: " + copyHashes.size());
+		}
+	}
+	
+	private Object[] getRandomReplacement() {
+		Object[] random = new Object[replacements.length];
+		for (int i = 0; i < replacements.length; i++) {
+			random[i] = replacements[i][(int) (Math.random() * replacements[i].length)];
+		}
+		return random;
+	}
+	
+	private static String readFile(String path) throws IOException {
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int)file.length()];
+		fis.read(data);
+		fis.close();
+		
+		return new String(data, "UTF-8");
+	}
+	
+	
+	
+	private static String[][] readReplacements() throws IOException {
 		List<String[]> repList = new ArrayList<String[]>();
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("Replacements.txt"), "UTF8"));
@@ -30,47 +128,11 @@ public class Generator {
 		return repList.toArray(new String[repList.size()][]);
 	}
 	
-	public static void main(String[] args) throws IOException {
-		String[][] replacements = readReplacements();
-		
-		File file = new File("OriginalFile.txt");
-		FileInputStream fis = new FileInputStream(file);
-		byte[] data = new byte[(int)file.length()];
-		fis.read(data);
-		fis.close();
-		
-		byte[] originalHash = HashAlgorithm.calculateHash(data);
-		
-		file = new File("PlaceHolderFile.txt");
-		fis = new FileInputStream(file);
-		byte[] dataPlaceHolder = new byte[(int)file.length()];
-		fis.read(dataPlaceHolder);
-		fis.close();
-		
-		String placeholder = new String(dataPlaceHolder, "UTF-8");
-		System.out.println(placeholder);
-		System.out.println(getCollisions(originalHash, placeholder, replacements, new String[replacements.length], 0, 0));
-	}
-	
-	public static int getCollisions(byte[] originalHash, String placeholder, String[][] replacements, String[] usedReplacements, int position, int count) throws FileNotFoundException {
-		if (position < replacements.length) {
-			int newCount = 0;
-			for (int i = 0; i < replacements[position].length; i++) {
-				usedReplacements[position] = replacements[position][i];
-				newCount += getCollisions(originalHash, placeholder, replacements, usedReplacements, position+1, count + newCount);
-			}
-			return newCount;
-		} else {
-			String newText = String.format(placeholder, (Object[])usedReplacements);
-			byte[] newHash = HashAlgorithm.calculateHash(newText.getBytes());
-			if (Arrays.areEqual(newHash, originalHash)) {
-				PrintWriter out = new PrintWriter("copy" + count + ".txt");
-				out.print(newText);
-				out.close();
-				return 1;
-			} else {
-				return 0;
-			}
+	public static void main(String[] args) {
+		try {
+			new Generator(2,true);
+		} catch (IOException e) {
+			System.out.println("Error accessing Files: " + e.getMessage());
 		}
 	}
 }
